a8e4e0d0116fd1db385a5727a7d31bd7af47533b
Max <66183307+maxverwiebe@users.noreply.github.com>
2025-04-17 10:33:31 +0200

    Update ToastContainer.js

--- components/ToastContainer.js
+++ components/ToastContainer.js
@@ -1,74 +1,53 @@
-import React from "react";
-import { useToast } from "@/lib/ToastContext";
+import React, { useState, useEffect } from "react";
+import { toastEmitter } from "@/lib/Toast";
+import { IoMdNotifications } from "react-icons/io";
+import { FaCheckCircle, FaTimesCircle } from "react-icons/fa";
 
-function Icon({ type }) {
-  switch (type) {
-    case "success":
-      return (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={2}
-          stroke="currentColor"
-          className="w-5 h-5 mr-2 text-green-500"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M5 13l4 4L19 7"
-          />
-        </svg>
-      );
-    case "error":
-      return (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={2}
-          stroke="currentColor"
-          className="w-5 h-5 mr-2 text-red-500"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M6 18L18 6M6 6l12 12"
-          />
-        </svg>
-      );
-    case "info":
-    default:
-      return (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={2}
-          stroke="currentColor"
-          className="w-5 h-5 mr-2 text-blue-500"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M13 16h-1v-4h-1m1-4h.01M12 20.5a8.5 8.5 0 100-17 8.5 8.5 0 000 17z"
-          />
-        </svg>
-      );
+function ToastIcon({ toastType }) {
+  if (toastType === "info") {
+    return <IoMdNotifications className="text-violet-500 mr-2 text-md" />;
   }
+
+  if (toastType === "success") {
+    return <FaCheckCircle className="text-green-600 mr-2 text-md" />;
+  }
+
+  if (toastType === "error") {
+    return <FaTimesCircle className="text-red-700 mr-2 text-md" />;
+  }
+
+  return <IoMdNotifications className="text-violet-500 mr-2 text-md" />;
 }
 
 export default function ToastContainer() {
-  const { toasts } = useToast();
+  const [toasts, setToasts] = useState([]);
+
+  useEffect(() => {
+    const handleAddToast = (event) => {
+      const newToast = event.detail;
+      setToasts((prevToasts) => [...prevToasts, newToast]);
+
+      setTimeout(() => {
+        setToasts((prevToasts) =>
+          prevToasts.filter((toast) => toast.id !== newToast.id)
+        );
+      }, newToast.duration);
+    };
+
+    toastEmitter.addEventListener("addToast", handleAddToast);
+    return () => {
+      toastEmitter.removeEventListener("addToast", handleAddToast);
+    };
+  }, []);
 
   return (
     <div className="fixed bottom-4 right-4 z-50 flex flex-col gap-2">
       {toasts.map((toast) => (
         <div
           key={toast.id}
-          className="flex items-center px-4 py-2 rounded shadow-md text-neutral-700 bg-gray-100 ease-in duration-700"
+          className="flex items-center px-4 py-2 rounded shadow-md text-neutral-700 bg-gray-100 transition duration-700"
         >
-          <Icon type={toast.type} />
+          <ToastIcon toastType={toast.type}></ToastIcon>
           <span>{toast.message}</span>
         </div>
       ))}
