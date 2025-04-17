2ff6da6914c67aa4120de8bbb395b03af4754391
Max <66183307+maxverwiebe@users.noreply.github.com>
2025-04-17 10:35:03 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,11 @@
 import "@/styles/globals.css";
-import { ToastProvider } from "@/lib/ToastContext";
 import ToastContainer from "@/components/ToastContainer";
 
 export default function App({ Component, pageProps }) {
   return (
-    <ToastProvider>
+    <>
       <Component {...pageProps} />
       <ToastContainer />
-    </ToastProvider>
+    </>
   );
 }
