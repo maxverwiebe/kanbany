98fbed667e23922dfc27c3360e00c209820a05c7
maxverwiebe <summe.business@gmail.com>
2025-04-18 08:33:57 +0200

    Update background color for dark mode in _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en" className="dark:bg-gray-900">
+    <Html lang="en" className="dark:bg-neutral-900">
       <Head />
       <body className="antialiased bg-background">
         <Main />
